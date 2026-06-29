6162c38e6131ede2d49953fe13971998270f063b
masaki-oonishi <masaki.oonishi@metamoji.com>
2026-06-29 17:00:58 +0900

    計算履歴ã保持数ãの定数をstaticにç·変更

--- app/src/main/java/com/example/calculator/model/HistoryManager.java
+++ app/src/main/java/com/example/calculator/model/HistoryManager.java
@@ -5,7 +5,7 @@ import java.util.Deque;
 import java.util.List;
 
 public class HistoryManager {
-    private final int MAX_HISTORY_COUNT = 10;
+    private static final int MAX_HISTORY_COUNT = 10;
     private final Deque<CalculationMemento> history = new ArrayDeque<>();
 
     public void addHistory(CalculationMemento memento){
